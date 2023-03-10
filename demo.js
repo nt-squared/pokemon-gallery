64b5700cf5ab4d8256e1985395d8b74fc22ec763
nt-squared <ngoctamnguyen3147@gmail.com>
2023-03-10 14:00:34 +0700

    change file name

--- demo.js
+++ demo.js
@@ -1,145 +1,174 @@
-const buttons = document.querySelectorAll('button');
-const pokeBox = document.querySelector('#poke-box');
-const spinner = document.querySelector('.spinner');
+const input = document.querySelector('input')
+const buttons = document.querySelectorAll('button')
+const pokeBox = document.querySelector('#poke-box')
+const pokeRegion = document.querySelector('#poke-region')
 
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?';
+// to get {"front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",}
 
-async function getPokemonEntries() {
-    let url = 'https://pokeapi.co/api/v2/generation/'
-    const res = await fetch(url)
-    const data = await res.json();
+document.addEventListener("DOMContentLoaded", () => {
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            // add region name
+            getPokemonRegion(button.id)
+                .then(regionName => addRegionName(regionName))
 
-    const totalGens = data.count;
-    let entriesArr = []
-    for (let i = 0; i < totalGens; i++) {
-        const res = await fetch(`${url}${i + 1}`)
-        const pokeGen = await res.json();
-        const pokeGenEntries = pokeGen.pokemon_species.length;
-        entriesArr.push(pokeGenEntries)
-    }
-
-    return (entriesArr);
-}
-const LOCAL_STORAGE_KEY = 'POKE-GEN';
-
-buttons.forEach((button, index) => {
-    let url = BASE_URL;
-    let offset = 0;
-
-    button.addEventListener('click', async () => {
-        pokeBox.innerHTML = "";
-
-
-
-        const entriesArr = await getPokemonEntries();
-        let limit = entriesArr[index];
-
-        if (index !== 0) {
-            let newEntriesArr = entriesArr.slice(0, index);
-            offset = newEntriesArr.reduce((acc, curr) => acc + curr, 0);
-        }
-
-        console.log(offset, limit);
-        const res = await fetch(`${url}offset=${offset}&limit=${limit}`);
-        const allPokemon = await res.json();
-
-
-        let fetchedData = [];
-        const resultsLength = allPokemon.results.length;
-
-        if (localStorage[`${LOCAL_STORAGE_KEY} ${index + 1}`]) {
-            console.log('get');
-            let storage = JSON.parse(localStorage.getItem(`${LOCAL_STORAGE_KEY} ${index + 1}`))
-            storage.forEach(renderPokemon);
-            return
-        }
-
-
-        console.log('set');
-        spinner.style.display = 'block';
-        for (let i = 0; i < resultsLength; i++) {
-            const pokeData = await fetchPokemonData(allPokemon.results[i]);
-            fetchedData.push(pokeData)
-        }
-
-        localStorage.setItem(`${LOCAL_STORAGE_KEY} ${index + 1}`, JSON.stringify(fetchedData))
-
-        fetchedData.forEach(renderPokemon);
-
-        spinner.style.display = 'none';
-
-
+            // empty content when load data
+            pokeBox.textContent = '';
+            getGenerationLength()
+                .then(data => {
+                    Promise.all(data)
+                        .then(data => {
+                            let [offset, limit] = getOffsetAndLimit(button.id, data);
+                            return getPokemonsOfSpecificGeneration(offset, limit);
+                        })
+                        .then(data => {
+                            let arrayUrls = getPokemonData(data);
+                            let arraysPokeInfo = getPokemonInfo(arrayUrls);
+                            // console.log(arraysPokeInfo);
+                            Promise.all(arraysPokeInfo)
+                                .then(arraysPokeInfo => {
+                                    arraysPokeInfo.forEach(arrayPokeInfo => {
+                                        renderPokemon(arrayPokeInfo)
+                                    })
+                                })
+                        });
+                });
+        })
     })
-
 })
 
 
-async function fetchPokemonData(pokemon) {
-    let url = pokemon.url;
-    const res = await fetch(url);
-    const { name, sprites, types } = await res.json();
+// get pokemon regions
+function getPokemonRegion(id) {
+    return fetch(`https://pokeapi.co/api/v2/generation/${id}`)
+        .then(async res => await res.json())
+        .then(data => data['main_region']['name'])
+}
 
-    return {
-        name,
-        image: sprites?.other['official-artwork']['front_default'],
-        types,
+// add pokemon region to box container
+function addRegionName(regionName) {
+    pokeRegion.textContent = regionName + " Region";
+}
+
+// Get total numbers of pokemons from each generation and put it into an array
+function getGenerationLength() {
+    return fetch(`https://pokeapi.co/api/v2/generation/`)
+        .then(async res => await res.json())
+        .then((data) => {
+            const { results } = data;
+            const arraySpecies = [];
+            results.forEach(result => {
+                let length = fetch(result.url)
+                    .then(res => res.json())
+                    .then(data => data['pokemon_species'].length)
+                arraySpecies.push(length)
+            })
+            return arraySpecies
+        })
+}
+
+// Get offset and limit of every generation
+function getOffsetAndLimit(gen, array) {
+    let sum = 0;
+    // total pokemons of previous generation compared to the gen variable
+    let limit;
+
+    for (let i = 0; i < array.length; i++) {
+        if (i === (gen - 1)) limit = array[i]
     }
+
+    for (let i = 0; i < gen - 1; i++) {
+        sum += array[i]
+    }
+
+    return [sum, limit]
 }
 
-// Render All Pokemon
-function renderPokemon({ types, image, name }) {
-
-    // Create Poke Card
-    let pokeCard = document.createElement('div');
-    pokeCard.className = 'poke-card'; // add class name
-
-    let typeList = createPokemonTypes(types);
-    let pokeImage = createPokemonImage(image)
-    let pokeName = createPokemonName(name)
-
-    pokeCard.append(typeList); // add type list
-    pokeCard.append(pokeName); // add name
-    pokeCard.append(pokeImage); // add image
-
-    pokeBox.append(pokeCard) // add poke card
+function getPokemonsOfSpecificGeneration(offset, limit) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+        .then(async res => await res.json())
+        .then((speciesData) => speciesData)
 }
 
-// Handle Pokemon Name
-function createPokemonName(name) {
-    let pokeName = document.createElement('h4');
-    pokeName.className = 'poke-name';
-    pokeName.innerHTML = name
-    return pokeName
-}
-
-// Handle Pokemon Image
-function createPokemonImage(image) {
-    let img = document.createElement('img');
-    img.src = image;
-    return img
-}
-
-// Handle Pokemon Type
-function createPokemonTypes(types) {
-    let ul = document.createElement('ul');
-    ul.className = 'type-list';
-
-    types.forEach(type => {
-        let li = document.createElement('li');
-        li.innerText = type?.type['name'];
-        getTypeColor(li);
-        ul.append(li)
+// Get url of every pokemon of a generation return an array of urls
+function getPokemonData({ results }) {
+    const arrayUrls = [];
+    results.forEach(result => {
+        if (result.url) {
+            let url = result.url;
+            arrayUrls.push(url)
+        }
     })
+    return arrayUrls;
+}
 
-    return ul
+// Fetch url of every pokemon of a generation and return name / image / type 
+function getPokemonInfo(url) {
+    const arrayPokeUrls = url;
+    const arraysPokeInfo = [];
+    arrayPokeUrls.forEach(arrayPokeUrl => {
+        let pokeInfo = fetch(arrayPokeUrl)
+            .then(async res => await res.json())
+            .then((data) => {
+                // console.log(data);
+                return {
+                    name: data.name,
+                    image: data.sprites.other['official-artwork'].front_default,
+                    types: data.types
+                }
+            })
+        arraysPokeInfo.push(pokeInfo)
+    })
+    return arraysPokeInfo;
+}
+
+// Use information after fetch to create a card of each pokemon 
+function renderPokemon({ name, image, types }) {
+    // div tag for pokemon container
+    const div = document.createElement('div');
+    div.classList.add('container')
+
+    // p tag for pokemon name
+    const p = document.createElement('p');
+    p.textContent = name;
+
+    // ul & li tags for pokemon types
+    const ul = document.createElement('ul');
+    createTypes(types, ul);
+
+    // img tag for pokemon image
+    const img = document.createElement('img');
+    img.src = image;
+
+    // add all tags to its container
+    div.append(p, ul, img)
+
+    // add all containers to a box
+    pokeBox.append(div);
+}
+
+function createTypes(types, ul) {
+    types.forEach(type => {
+        // create li tag
+        let typeLi = document.createElement('li');
+
+        // add content to li tag
+        typeLi.innerText = type['type']['name'];
+
+        // add background color to the tag
+        // by classifying types
+        getTypeColor(typeLi);
+
+        // add li tag to ul tag
+        ul.append(typeLi);
+    })
 }
 
 function getTypeColor(type) {
-    typeColors.forEach(typeColor => {
-        let [pokeType] = Object.keys(typeColor);
-
-        if (pokeType === type.innerHTML) {
-            type.style.backgroundColor = typeColor[pokeType];
+    typeColors.forEach(color => {
+        const [key] = Object.keys(color)
+        if (key === type.innerText) {
+            type.style.backgroundColor = color[key];
         }
     })
 }
@@ -163,5 +192,4 @@ const typeColors = [
     { dragon: '#6f43f3' },
     { steel: '#b8b9cf' },
     { fairy: '#efb6bc' }
-];
-
+]
